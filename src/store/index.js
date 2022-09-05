f7ab864d840024973ec1e66f7249e9a7d6192b58
一如年少模样 <10610167+qaz0924@user.noreply.gitee.com>
2022-09-05 21:04:19 +0800

    初始化项目

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,13 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  //  state看做 data 数据
+  state: {},
+  // 可以看作 computed 计算属性
+  getters: {},
+  // 可以看作 methods 方法,更改 Vuex 中的 state 的唯一方法只有提交 mutation
+  mutations: {},
+  // actions用来处理mutations中的异步操作，触发mutations中的函数
+  actions: {},
+  modules: {}
 })
